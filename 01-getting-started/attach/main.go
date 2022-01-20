c6772c34d78968e998bdf0f3a64cca70f7092d46
Derek Parker <parkerderek86@gmail.com>
2022-01-20 15:46:37 -0800

    Add server port to attach program output

--- 01-getting-started/attach/main.go
+++ 01-getting-started/attach/main.go
@@ -11,5 +11,6 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, %q\n", html.EscapeString(r.URL.Path))
 	})
+	fmt.Println("Server started on :8081")
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
